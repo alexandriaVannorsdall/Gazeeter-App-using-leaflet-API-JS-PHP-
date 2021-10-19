836711ebc9c0fed7feb87dc3ae130889c9e043fc
Alexandria Vannorsdall <arvannorsdall@yahoo.com>
2021-10-19 10:18:47 +0100

    second commit

---
+++ libs/php/countryCodeTimeWeather.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$executionStartTime = microtime(true);
+
+$url="http://api.geonames.org/countryCodeJSON?formatted=true&lat=47.03&lng=10.2&username=demo&style=full";
+
+
+
+
+?>
