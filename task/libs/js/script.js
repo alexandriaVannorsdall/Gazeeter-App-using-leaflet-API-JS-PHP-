d052199fa0d29c53bd01d59dd02bbc4a76c7b5d7
Alexandria Vannorsdall <arvannorsdall@yahoo.com>
2021-10-20 12:02:42 +0100

    added some more table elements

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -1,12 +1,27 @@
 $('submit').click(function() {
 
     $.$.ajax({
-        url: "libs/php/countryCodeTimeWeath.php",
-        type: "POST",
+        url: "libs/php/countryCodeTimeWeather.php",
+        type: 'POST',
         dataType: 'json',
-        data: "data",
+        data: {
+            lat: $('#lat').val(),
+            lng: $('#lng').val()
+        },
         success: function (response) {
+            console.log(JSON.stringfy(response));
+
+            if (response.status.name == "ok") {
+
+                $('#txtCountry').html(result['data'][0]['country']);
+                $('#txtTimezone').html(result['data'][0]['timezone']);
+                $('#txtWeather').html(result['data'][0]['weather']);
+
+            }
             
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("There has been an error.");
         }
     });
 });
