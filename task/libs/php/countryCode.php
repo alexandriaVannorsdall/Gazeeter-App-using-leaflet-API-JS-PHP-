10ca22333cfc8f128381be3fc54bd056235f7b3b
Alexandria Vannorsdall <arvannorsdall@yahoo.com>
2021-10-25 11:45:58 +0100

    countryCode.php works

--- task/libs/php/countryCode.php
+++ task/libs/php/countryCode.php
@@ -20,7 +20,7 @@ $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
 $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output['data'] = $decode['http://api.geonames.org/countryCode?lat=47.03&lng=10.2&username=Aless81'];
+$output['data'] = $decode['countryCode'];
 
 
 	
--- task/libs/php/timezone.php
+++ task/libs/php/timezone.php
@@ -20,7 +20,7 @@ $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
 $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output['data'] = $decode['geonames'];
+$output['data'] = $decode['timezone'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
